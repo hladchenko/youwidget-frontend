438f823c826d1dc17def0e7763d65b0a122ecb74
Ruslan Hladchenko <rugladchenko@gmail.com>
2025-09-17 10:14:46 +0300

    Add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home.tsx";
-import About from "@/pages/About.tsx";
+import Dashboard from "@/pages/Dashboard.tsx";
+import DashboardLayout from "@/layouts/DashboardLayout.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div>About</div>;
+};
+
+export default Dashboard;
