ca4f338fcec4a51fb1075907b7ab35501b725dec
Ruslan Hladchenko <rugladchenko@gmail.com>
2025-09-17 18:41:17 +0300

    Change dashboard bg color and align headings

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 interface HeadingProps {
   pageName: string;
 }
 
 const PageHeading = ({ pageName }: HeadingProps) => {
   return (
-    <div className="my-10 sm:flex sm:items-center sm:justify-between">
+    <div className="mb-5 mt-6 sm:flex sm:items-center sm:justify-between">
       <div className="min-w-0 flex-1">
         <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-2xl sm:tracking-tight">
           {pageName}
