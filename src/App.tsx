3645395ae65de6355c5fe9ca7c439a8ca64107fe
Ruslan Hladchenko <rugladchenko@gmail.com>
2025-09-17 10:08:20 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home.tsx";
+import About from "@/pages/About.tsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
