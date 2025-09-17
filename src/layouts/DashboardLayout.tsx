70181a49e77208c42a90d31422dc04cd293abf08
Ruslan Hladchenko <rugladchenko@gmail.com>
2025-09-17 22:06:35 +0300

    Change menu size and fix GitHub actions

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -111,7 +111,7 @@ const DashboardLayout = () => {
         </Dialog>
 
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-gray-50 px-6">
             <div className="flex h-16 shrink-0 items-center">
@@ -181,7 +181,7 @@ const DashboardLayout = () => {
           </a>
         </div>
 
-        <main className="pb-10 pt-2 lg:pl-72">
+        <main className="pb-10 pt-2 lg:pl-64">
           <div className="px-4 sm:px-6 lg:px-8">
             <Outlet />
           </div>
