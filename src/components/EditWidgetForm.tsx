b6b1217b7beaa67c1e318dc6abfc51b1828fa651
Ruslan Hladchenko <rugladchenko@gmail.com>
2025-09-24 14:06:50 +0300

    Add react hook form

--- src/components/EditWidgetForm.tsx
+++ src/components/EditWidgetForm.tsx
@@ -1,49 +1,66 @@
-const EditWidgetForm = () => {
-  return (
-    <>
-      <form className="w-full">
-        <div>
-          <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-4">
-            <div>
-              <label
-                htmlFor="title"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Title
-              </label>
-              <div className="mt-2">
-                <input
-                  id="title"
-                  name="title"
-                  type="text"
-                  placeholder="Widget Title"
-                  aria-describedby="title-description"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
+import { useForm } from "react-hook-form";
 
-            <div className="col-span-full">
-              <label
-                htmlFor="text"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Text
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="text"
-                  name="text"
-                  rows={3}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  defaultValue={""}
-                />
-              </div>
+const EditWidgetForm = () => {
+  type Inputs = {
+    title: string;
+    text: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit = (values: Inputs) => {
+    console.log(values);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+      <div>
+        <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-2">
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Title
+            </label>
+            <div className="mt-2">
+              <input
+                id="title"
+                type="text"
+                placeholder="Widget Title"
+                aria-describedby="title-description"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                {...register("title", { required: true })}
+              />
+            </div>
+            <p className="mt-2">
+              {errors.title && <span>This field is required</span>}
+            </p>
+          </div>
+
+          <div className="col-span-full">
+            <label
+              htmlFor="text"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Text
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="text"
+                rows={3}
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                defaultValue={""}
+                {...register("text")}
+              />
             </div>
           </div>
         </div>
-      </form>
-    </>
+      </div>
+    </form>
   );
 };
 
